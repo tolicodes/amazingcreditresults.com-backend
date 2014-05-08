a68cf9147de065f85b2c75583d35001428bc1bc6
Anatoliy Ostroumov <ostroumov095@gmail.com>
2014-05-08 23:53:07 +0400

    owner endpoint for editing users

--- controllers/owner/editClients.js
+++ controllers/owner/editClients.js
@@ -8,6 +8,16 @@
 
 
 module.exports = exports = function(core){
+
+var ensureAdmin = function(request, response, next){
+  if(request.user && request.user.root){
+    next();
+  } else {
+    request.flash('error','Authentication required!');
+    response.redirect('/admin/login');
+  }
+}
+
 /**
  * @swagger
  * path: /admin/clients
@@ -15,7 +25,7 @@ module.exports = exports = function(core){
  *   -  httpMethod: GET
  *      nickname: Owner dashboard for editing clients
  */
-  core.app.get('/admin/clients', function(request, response){
+  core.app.get('/admin/clients', ensureAdmin, function(request, response){
     response.send('clients')
   });
 
@@ -26,43 +36,39 @@ module.exports = exports = function(core){
  *   -  httpMethod: GET
  *      nickname: Owners' json endpoing with data about clients
  */
-  core.app.get('/admin/clients.json', function(request, response){
-    if(request.user && request.user.root){
-      request.model.User
-        .find({
-
-        })
-        .limit(100)
-        .skip()
-        .exec(function(error, usersFound){
-          if(error){
-            throw error;
-          } else {
-            var usersPrepared = usersFound.map(function(user){
-              return {
-                "id": user.id,
-                "email": user.email,
-                "name": {
-                  "familyName" : user.name.familyName,
-                  "givenName" : user.name.givenName,
-                  "middleName" : user.name.middleName,
-                },
-                "gravatar": user.gravatar,
-                "gravatar30": user.gravatar30,
-                "gravatar50": user.gravatar50,
-                "gravatar80": user.gravatar80,
-                "gravatar100":  user.gravatar100,
-                "online": user.online,
-                "root":user.root,
-
-              }
-            });
-            response.json(usersPrepared);
-          }
-        });
-    } else {
-      response.send(403);
-    }
+  core.app.get('/admin/clients.json', ensureAdmin, function(request, response){
+    request.model.User
+      .find({
+         //todo - parameters for limiting output
+      })
+      .limit(100)
+      .skip() //todo - pagination
+      .exec(function(error, usersFound){
+        if(error){
+          throw error;
+        } else {
+          var usersPrepared = usersFound.map(function(user){
+            return {
+              "id": user.id,
+              "email": user.email,
+              "name": {
+                "familyName" : user.name.familyName,
+                "givenName" : user.name.givenName,
+                "middleName" : user.name.middleName,
+              },
+              "gravatar": user.gravatar,
+              "gravatar30": user.gravatar30,
+              "gravatar50": user.gravatar50,
+              "gravatar80": user.gravatar80,
+              "gravatar100":  user.gravatar100,
+              "online": user.online,
+              "root": user.root,
+              "accountVerified": user.accountVerified
+            }
+          });
+          response.json({ 'page':1,'clients':usersPrepared});
+        }
+      });
   });
 
 /**
@@ -72,9 +78,14 @@ module.exports = exports = function(core){
  *   -  httpMethod: GET
  *      nickname: Owners json with data about clients
  */
-  core.app.get('/admin/clients/:id', function(request, response){});
+  core.app.get('/admin/clients/:id', ensureAdmin, function(request, response){
 
-  core.app.put('/admin/clients/:id', function(request, response){});
+  });
 
-  core.app.post('/admin/clients', function(request, response){});
+  core.app.put('/admin/clients/:id', ensureAdmin, function(request, response){});
+
+  core.app.post('/admin/clients', ensureAdmin, function(request, response){});
+
+//send message with link to site
+  core.app.post('/admin/clients/notify/:id', ensureAdmin,function(request, response){});
 };
