b18d93b75b7910ce42e9e1e6d6a1e62ba75d44be
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-05-10 23:04:24 +0400

    api endpoint for saving users answers to questions

--- controllers/buyer/landing.js
+++ controllers/buyer/landing.js
@@ -8,8 +8,33 @@ module.exports = exports = function(core){
 
 //save buyer answers to questionare - we recieve POST, we respond with JSON
   core.app.post('/buyer/saveQuestionareAnswers', function(request, response){
-
+    if(request.user){
+      request.user.profile = request.user.profile || {};
+      request.user.profile.answer1 = request.body.answer1;
+      request.user.profile.answer2 = request.body.answer2;
+      request.user.profile.answer3 = request.body.answer3;
+      request.user.save(function(error, userSaved){
+        if(error){
+          throw error;
+        } else {
+          response.status(202);
+          response.json({
+            'id': userSaved.id,
+            'email': userSaved.email,
+            'name':{
+              'givenName':userSaved.name.givenName,
+              'middleName':userSaved.name.middleName,
+              'familyName':userSaved.name.familyName
+            },
+            'profile': userSaved.profile,
+            'root': false,
+            'accountVerified': userSaved.accountVerified
+          });
+        }
+      });
+    } else {
+      response.status(400);
+      response.json({'error':'Authorization is required'})
+    }
   });
-
-//save buyer profile
 };
