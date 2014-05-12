449afa1965be12af259742f5f0b6d8900c47e0cc
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-05-12 22:15:30 +0400

    https://oselot.atlassian.net/browse/ACR-113

--- controllers/owner/login.js
+++ controllers/owner/login.js
@@ -24,5 +24,26 @@ module.exports = exports = function(core){
     response.redirect('/admin/login');
   });
 
-}
+
+  core.app.post('/api/v1/owner/login', function(request, response){
+    if(request.body.username && request.body.password){
+      request.model.User.findOneByEmail(request.body.username, function(error, userFound){
+        if(error){
+          throw error;
+        } else {
+          if(userFound && userFound.root){
+            response.status(200);
+            response.json({'Code':200, 'huntKey':userFound.apiKey})
+          } else {
+            response.status(403);
+            response.json({'Code':403,'Error':'Unable to authorize Owner with this credentials!'});
+          }
+        }
+      });
+    } else {
+      response.status(400);
+      response.json({'Code':400,'Error':'The values of `username` or `password` are not provided!'});
+    }
+  });
+};
 
