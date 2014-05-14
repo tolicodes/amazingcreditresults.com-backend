4247425bb1483b9fc820fbb0db0761ec125c7ebe
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-05-14 17:55:26 +0400

    https://oselot.atlassian.net/browse/ACR-134

--- controllers/shared.js
+++ controllers/shared.js
@@ -4,24 +4,27 @@ module.exports = exports = function (core) {
     if (request.user) {
       var user = request.user;
       response.json({
-        "id": user.id,
-        "huntKey": user.apiKey,//used for sessionless authorization
-        "email": user.email,
-        "name": {
-          "familyName": user.name.familyName, //http://schema.org/familyName
-          "givenName": user.name.givenName, //http://schema.org/givenName
-          "middleName": user.name.middleName //http://schema.org/middleName - at least the google oauth has this structure!
+        'id': user.id,
+        'huntKey': user.apiKey,//used for sessionless authorization
+        'email': user.email,
+        'name': {
+          'familyName': user.name.familyName, //http://schema.org/familyName
+          'givenName': user.name.givenName, //http://schema.org/givenName
+          'middleName': user.name.middleName //http://schema.org/middleName - at least the google oauth has this structure!
         },
-        "gravatar": user.gravatar,
-        "gravatar30": user.gravatar30,
-        "gravatar50": user.gravatar50,
-        "gravatar80": user.gravatar80,
-        "gravatar100": user.gravatar100,
-        "online": user.online,
-        "root": user.root,
-        "accountVerified": user.accountVerified,
-        "telefone": user.profile ? user.profile.telefone : '',
-        "localAddress": user.profile ? user.profile.localAddress : ''
+        'gravatar': user.gravatar,
+        'gravatar30': user.gravatar30,
+        'gravatar50': user.gravatar50,
+        'gravatar80': user.gravatar80,
+        'gravatar100': user.gravatar100,
+        'online': user.online,
+        'root': user.root,
+        'accountVerified': user.accountVerified,
+        'telefone': user.profile ? user.profile.telefone : '',
+        'localAddress': user.profile ? user.profile.localAddress : '',
+        'profile': {
+          'needQuestionnaire': user.profile ? user.profile.needQuestionnaire : true
+        }
       });
     } else {
       response.status(400);
