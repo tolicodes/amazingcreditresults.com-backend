0dac99d97317d612d151b8c38565828719e03d6d
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-05-23 23:52:09 +0400

    fixed test in previous commit

--- test/api_v1_buyer_login.test.js
+++ test/api_v1_buyer_login.test.js
@@ -2,12 +2,12 @@ var request = require('request'),
   url = require('url'),
   should = require('should'),
   port = process.env.PORT || 3000,
-  testId = Math.floor(Math.random()*10000),
+  testId = Math.floor(Math.random() * 10000),
   ownerHuntKey,
   buyerHuntKey,
   userId;
 
-describe('Unit test for user authorization by welcome link', function() {
+describe('Unit test for user authorization by welcome link', function () {
   it('allows owner to authorize at first', function (done) {
 
     request({
@@ -31,24 +31,24 @@ describe('Unit test for user authorization by welcome link', function() {
     });
   });
 
-  describe('creating user without verified account', function(){
-    before(function(done){
+  describe('creating user without verified account', function () {
+    before(function (done) {
       request({
-        'method':'POST',
-        'url':'http://localhost:'+port+'/api/v1/admin/clients',
+        'method': 'POST',
+        'url': 'http://localhost:' + port + '/api/v1/admin/clients',
         'headers': {'huntKey': ownerHuntKey},
-        'form':{
-          'email': 'unitTestUser'+testId+'@mail.ru',
-          'givenName': 'John'+testId,
-          'middleName': 'Teodor'+testId,
-          'familyName': 'Doe'+testId,
+        'form': {
+          'email': 'unitTestUser' + testId + '@mail.ru',
+          'givenName': 'John' + testId,
+          'middleName': 'Teodor' + testId,
+          'familyName': 'Doe' + testId,
           'needQuestionnaire': true,
-          'telefone': '555-339'+testId,
+          'telefone': '555-339' + testId,
           'localAddress': 'Some Address',
-          'title': 'Mr.',
+          'title': 'Mr.'
         }
-      }, function(error, response, body){
-        if(error) {
+      }, function (error, response, body) {
+        if (error) {
           done(error);
         } else {
           var bodyParsed = JSON.parse(body);
@@ -59,26 +59,26 @@ describe('Unit test for user authorization by welcome link', function() {
       });
     });
 
-    it('creates user without verified account', function(){
+    it('creates user without verified account', function () {
       userId.should.be.a.String;
       userId.should.match(/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i);
     });
 
-    it('notifies this user by email message', function(done){
+    it('notifies this user by email message', function (done) {
       request({
-        'method':'POST',
-        'url':'http://localhost:'+port+'/api/v1/admin/clients/welcome/'+userId,
-        'form':{ },
-        'headers': {'huntKey':ownerHuntKey}
-      }, function(error, response, body){
-        if(error) {
+        'method': 'POST',
+        'url': 'http://localhost:' + port + '/api/v1/admin/clients/welcome/' + userId,
+        'form': { },
+        'headers': {'huntKey': ownerHuntKey}
+      }, function (error, response, body) {
+        if (error) {
           done(error);
         } else {
           response.statusCode.should.be.equal(202);
           var bodyParsed = JSON.parse(body);
           bodyParsed.message.should.be.equal('sent');
           var params = url.parse(bodyParsed.welcomeLink);
-          ['http:','https:'].should.include(params.protocol);
+          ['http:', 'https:'].should.include(params.protocol);
           params.pathname.should.be.equal('/');
           params.hash.should.match(/^\#login\/[a-z]+$/);
           welcomeLink = (/^\#login\/([a-z]+)$/.exec(params.hash))[1];
@@ -88,13 +88,13 @@ describe('Unit test for user authorization by welcome link', function() {
       });
     });
 
-    it('makes this user to have correct response on /api/v1/api/v1/buyer/needToSetPassword/:welcomeLink', function(done){
+    it('makes this user to have correct response on /api/v1/api/v1/buyer/needToSetPassword/:welcomeLink', function (done) {
       request({
-        'method':'GET',
-        'url':'http://localhost:'+port+'/api/v1/buyer/needToSetPassword/'+welcomeLink,
+        'method': 'GET',
+        'url': 'http://localhost:' + port + '/api/v1/buyer/needToSetPassword/' + welcomeLink,
         'headers': { }
-      }, function(error, response, body){
-        if(error) {
+      }, function (error, response, body) {
+        if (error) {
           done(error);
         } else {
           response.statusCode.should.be.equal(200);
@@ -105,17 +105,17 @@ describe('Unit test for user authorization by welcome link', function() {
       });
     });
 //https://oselot.atlassian.net/browse/ACR-174
-    it('makes this user to have the correct response for setting password via /api/v1/buyer/setPassword', function(done){
+    it('makes this user to have the correct response for setting password via /api/v1/buyer/setPassword', function (done) {
       request({
-        'method':'POST',
-        'url':'http://localhost:'+port+'/api/v1/buyer/setPassword',
+        'method': 'POST',
+        'url': 'http://localhost:' + port + '/api/v1/buyer/setPassword',
         'headers': { },
-        'form':{
-          'apiKey':welcomeLink,
-          'password':'fiflesAndFufles'
+        'form': {
+          'apiKey': welcomeLink,
+          'password': 'fiflesAndFufles'
         }
-      }, function(error, response, body){
-        if(error) {
+      }, function (error, response, body) {
+        if (error) {
           done(error);
         } else {
           response.statusCode.should.be.equal(201);
@@ -124,106 +124,61 @@ describe('Unit test for user authorization by welcome link', function() {
           bodyParsed.Success.should.be.equal('Password is set!');
           done();
         }
-    });
-
-    it('makes this user to have correct response for authorizing via /api/v1/buyer/login', function(done){
-      request({
-        'method':'POST',
-        'url':'http://localhost:'+port+'/api/v1/buyer/login',
-        'headers': { },
-        'form':{
-          'apiKey':welcomeLink,
-          'password':'fiflesAndFufles'
-        }
-      }, function(error, response, body){
-        if(error) {
-          done(error);
-        } else {
-          response.statusCode.should.be.equal(201);
-          var bodyParsed = JSON.parse(body);
-          bodyParsed.Code.should.be.equal(201);
-          bodyParsed.Success.should.be.equal('Welcome!');
-          bodyParsed.huntKey.should.be.a.String;
-          buyerHuntKey = bodyParsed.huntKey;
-          done();
-        }
-    });
-  });
-
-  it('actually authorizes user via header based sessions', function(done){
-    request({
-      'method':'GET',
-      'url':'http://localhost:'+port+'/api/v1/myself',
-      'headers': {'huntKey':buyerHuntKey}
-    }, function(error, response, body){
-        response.statusCode.should.be.equal(200);
-        var bodyParsed = JSON.parse(body);
-        bodyParsed.id.should.be.equal(userId);
-        done();
-    });
-  });
-
-  it('makes this user to have correct response on /api/v1/api/v1/buyer/needToSetPassword/:welcomeLink',function(done){
-    request({
-      'method':'GET',
-      'url':'http://localhost:'+port+'/api/v1/buyer/needToSetPassword/'+welcomeLink,
-      'headers': { }
-    }, function(error, response, body){
-      if(error) {
-        done(error);
-      } else {
-        response.statusCode.should.be.equal(200);
-        var bodyParsed = JSON.parse(body);
-        bodyParsed.needToSetPassword.should.be.true;
-        done();
-      }
-    });
-  });
-
-/*/
-  describe('creating user with verified account', function(){
-    var bodyParsed;
-    before(function(done){
-      request({
-        'method':'PUT',
-        'url':'http://localhost:'+port+'/api/v1/admin/clients/'+userId,
-        'form':{
-          'accountVerified':true
-        },
-        'headers': {'huntKey':ownerHuntKey}
-      }, function(error, response, body){
-        if(error) {
-          done(error);
-        } else {
-          response.statusCode.should.be.equal(202);
-          bodyParsed = JSON.parse(body);
-          done();
-        }
       });
-    });
 
-    it('owner updates this user profile to be verified', function(){
-      bodyParsed.id.should.be.equal(userId);
-      bodyParsed.accountVerified.should.be.false;
-    });
+      it('makes this user to have correct response for authorizing via /api/v1/buyer/login', function (done) {
+        request({
+          'method': 'POST',
+          'url': 'http://localhost:' + port + '/api/v1/buyer/login',
+          'headers': { },
+          'form': {
+            'apiKey': welcomeLink,
+            'password': 'fiflesAndFufles'
+          }
+        }, function (error, response, body) {
+          if (error) {
+            done(error);
+          } else {
+            response.statusCode.should.be.equal(201);
+            var bodyParsed = JSON.parse(body);
+            bodyParsed.Code.should.be.equal(201);
+            bodyParsed.Success.should.be.equal('Welcome!');
+            bodyParsed.huntKey.should.be.a.String;
+            buyerHuntKey = bodyParsed.huntKey;
+            done();
+          }
+        });
+      });
 
-    it('makes this user to have correct response on /api/v1/api/v1/buyer/needToSetPassword/:welcomeLink',function(done){
-      request({
-        'method':'GET',
-        'url':'http://localhost:'+port+'/api/v1/buyer/needToSetPassword/'+welcomeLink,
-        'headers': { }
-      }, function(error, response, body){
-        if(error) {
-          done(error);
-        } else {
+      it('actually authorizes user via header based sessions', function (done) {
+        request({
+          'method': 'GET',
+          'url': 'http://localhost:' + port + '/api/v1/myself',
+          'headers': {'huntKey': buyerHuntKey}
+        }, function (error, response, body) {
           response.statusCode.should.be.equal(200);
           var bodyParsed = JSON.parse(body);
-          bodyParsed.needToSetPassword.should.be.true;
+          bodyParsed.id.should.be.equal(userId);
           done();
-        }
+        });
+      });
+
+      it('makes this user to have correct response on /api/v1/api/v1/buyer/needToSetPassword/:welcomeLink', function (done) {
+        request({
+          'method': 'GET',
+          'url': 'http://localhost:' + port + '/api/v1/buyer/needToSetPassword/' + welcomeLink,
+          'headers': { }
+        }, function (error, response, body) {
+          if (error) {
+            done(error);
+          } else {
+            response.statusCode.should.be.equal(200);
+            var bodyParsed = JSON.parse(body);
+            bodyParsed.needToSetPassword.should.be.true;
+            done();
+          }
+        });
       });
     });
-//*/
   });
-
 });
