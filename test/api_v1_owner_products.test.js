ac0ee23c1003d2c96d35316161a07af4fa22c0df
Anatoliy Ostroumov <ostroumov095@gmail.com>
2014-05-29 01:18:28 +0400

    https://oselot.atlassian.net/browse/ACR-199

--- test/api_v1_owner_products.test.js
+++ test/api_v1_owner_products.test.js
@@ -1,6 +1,7 @@
 var request = require('request'),
   should = require('should'),
   port = process.env.PORT || 3000,
+  testId = Math.floor(Math.random()*10000),
   ownerHuntKey,
   productId;
 
@@ -27,9 +28,68 @@ describe('/api/v1/owner/products test', function(){
     });
   });
 
-  it('owner can create product');
+  it('owner can create product', function(done){
+    request({
+      'method':'POST',
+      'url':'http://localhost:'+port+'/api/v1/owner/products',
+      'headers': { 'huntKey':ownerHuntKey },
+      'form' : {
+        'name': 'SuperMega'+testId,
+        'bank': 'SuperMegaBank'+testId,
+        'type': 'MasterCard',
+        'ncRating': 'None',
+        'bcRating': 'Bronze',
+        'moRating': 'Silver',
+        'reportsToExperian': false,
+        'reportsToEquifax': false,
+        'reportsToTransunion': false
+      }
+    }, function(error, response, body){
+      if(error) {
+        done(error);
+      } else {
+        response.statusCode.should.be.equal(201);
+        var bodyParsed = JSON.parse(body);
+        bodyParsed.data.name.should.be.equal('SuperMega'+testId);
+        bodyParsed.data.bank.should.be.equal('SuperMegaBank'+testId);
+        bodyParsed.data.type.should.be.equal('MasterCard');
+        bodyParsed.data.ncRating.should.be.equal('None');
+        bodyParsed.data.bcRating.should.be.equal('Bronze');
+        bodyParsed.data.moRating.should.be.equal('Silver');
+        bodyParsed.data.reportsToExperian.should.be.false;
+        bodyParsed.data.reportsToEquifax.should.be.false;
+        bodyParsed.data.reportsToTransunion.should.be.false;
+        productId = bodyParsed.data.id;
+        done();
+      }
+    });
+  });
   it('owner can list products');
-  it('owner can list one product');
+  it('owner can list one product', function(done){
+    request({
+      'method':'GET',
+      'url':'http://localhost:'+port+'/api/v1/owner/products/'+productId,
+      'headers': { 'huntKey':ownerHuntKey }
+    }, function(error, response, body){
+      if(error) {
+        done(error);
+      } else {
+        response.statusCode.should.be.equal(200);
+        var bodyParsed = JSON.parse(body);
+        bodyParsed.data.id.should.be.equal(productId);
+        bodyParsed.data.name.should.be.equal('SuperMega'+testId);
+        bodyParsed.data.bank.should.be.equal('SuperMegaBank'+testId);
+        bodyParsed.data.type.should.be.equal('MasterCard');
+        bodyParsed.data.ncRating.should.be.equal('None');
+        bodyParsed.data.bcRating.should.be.equal('Bronze');
+        bodyParsed.data.moRating.should.be.equal('Silver');
+        bodyParsed.data.reportsToExperian.should.be.false;
+        bodyParsed.data.reportsToEquifax.should.be.false;
+        bodyParsed.data.reportsToTransunion.should.be.false;
+        done();
+      }
+    });
+  });
   it('owner can update product');
   it('owner can delete product with now tradelines associated');
 });
--- test/api_v1_owner_tradelines.test.js
+++ test/api_v1_owner_tradelines.test.js
@@ -1,3 +1,35 @@
-describe('/api/v1/owner/products test', function(){
-  it('will be done!');
+var request = require('request'),
+  should = require('should'),
+  port = process.env.PORT || 3000,
+  ownerHuntKey,
+  tradeLineId;
+
+describe('/api/v1/owner/tradelines test', function(){
+  before(function(done){
+    request({
+      'method':'POST',
+      'url':'http://localhost:'+port+'/api/v1/owner/login',
+      'form' : {
+        'username':'owner@example.org',
+        'password':'test123'
+      }
+    }, function(error, response, body){
+      if(error) {
+        done(error);
+      } else {
+        response.statusCode.should.be.equal(200);
+        var bodyParsed = JSON.parse(body);
+        bodyParsed.Code.should.be.equal(200);
+        bodyParsed.huntKey.should.be.a.String;
+        ownerHuntKey=bodyParsed.huntKey;
+        done();
+      }
+    });
+  });
+
+  it('owner can create tradeline');
+  it('owner can list tradelines');
+  it('owner can list one tradeline');
+  it('owner can update tradeline');
+  it('owner can delete tradeline?');
 });
