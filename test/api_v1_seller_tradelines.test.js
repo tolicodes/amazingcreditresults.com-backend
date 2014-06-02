d4020cac76e71b9414fd44d859f388b89fddb40d
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-06-03 01:27:47 +0400

    https://oselot.atlassian.net/browse/ACR-213#

--- test/api_v1_seller_tradelines.test.js
+++ test/api_v1_seller_tradelines.test.js
@@ -1,13 +1,11 @@
 var request = require('request'),
   should = require('should'),
-  testId = Math.floor(Math.random()*10000),
+  testId = Math.floor(Math.random() * 10000),
   port = process.env.PORT || 3000;
-
-//dev seller has unchangable `huntKey`, restored on every backend start
-
 
 describe('Seller editing his/her tradelines', function () {
   var sellerId,
+    productId,
     sellerHuntKey,
     tradelineId;
 
@@ -58,9 +56,9 @@ describe('Seller editing his/her tradelines', function () {
       'method': 'POST',
       'url': 'http://localhost:' + port + '/api/v1/seller/tradelines',
       'headers': {'huntKey': sellerHuntKey},
-      'form':{
-        'name':'TestTradeline'+testId,
-        'product':'5366506291e1e82b0f4be503', //non existant, but valid id
+      'form': {
+        'name': 'TestTradeline' + testId,
+        'product': '5366506291e1e82b0f4be503', //non existant, but valid id
         'seller': sellerId,
         'totalAus': 10,
         'usedAus': 5,
@@ -68,11 +66,11 @@ describe('Seller editing his/her tradelines', function () {
         'creditLimit': 10000,
         'cashLimit': 10000,
         'currentBalance': 1000,
-        'ncRating':'Silver',
-        'bcRating':'Silver',
-        'moRating':'Silver',
-        'cost':1000,
-        'notes':'Some notes'
+        'ncRating': 'Silver',
+        'bcRating': 'Silver',
+        'moRating': 'Silver',
+        'cost': 1000,
+        'notes': 'Some notes'
       }
     }, function (error, response, body) {
       if (error) {
@@ -92,14 +90,158 @@ describe('Seller editing his/her tradelines', function () {
     });
   });
 
-  it('seller can create tradeline with existant product id');
+  describe('seller can create tradeline with existant product id', function () {
+    before(function (done) {
+      request({
+        'method': 'GET',
+        'url': 'http://localhost:' + port + '/api/v1/seller/products',
+        'headers': {'huntKey': sellerHuntKey }
+      }, function (error, response, body) {
+        if (error) {
+          done(error);
+        } else {
+          response.statusCode.should.be.equal(200);
+          var bodyParsed = JSON.parse(body);
+          Array.isArray(bodyParsed.data).should.be.true;
+          bodyParsed.data.map(function (product) {
+            product.id.should.be.a.String;
+            product.name.should.be.a.String;
+            product.bank.should.be.a.String;
+            product.type.should.be.a.String;
 
-  it('seller can list all his/her tradelines');
+            [
+              'MasterCard', 'Visa',
+              'American Express', 'Discover'
+            ].should.containEql(product.type);
 
-  it('seller can get one tradeline');
+            ['ncRating', 'bcRating', 'moRating'].map(function (r) {
+              ['None', 'Bronze', 'Silver', 'Gold'].should.containEql(product[r])
+            });
+            product.reportsToExperian.should.be.a.Boolean;
+            product.reportsToEquifax.should.be.a.Boolean;
+            product.reportsToTransunion.should.be.a.Boolean;
+            productId = product.id;
+          });
+          done()
+        }
+      });
+    });
+    it('works for POST response', function (done) {
+      request({
+        'method': 'POST',
+        'url': 'http://localhost:' + port + '/api/v1/seller/tradelines',
+        'headers': {'huntKey': sellerHuntKey},
+        'form': {
+          'name': 'TestTradeline' + testId,
+          'product': productId, //non existant, but valid id
+          'seller': sellerId,
+          'totalAus': 10,
+          'usedAus': 5,
+          'price': 1100,
+          'creditLimit': 10000,
+          'cashLimit': 10000,
+          'currentBalance': 1000,
+          'ncRating': 'Silver',
+          'bcRating': 'Silver',
+          'moRating': 'Silver',
+          'cost': 1000,
+          'notes': 'Some notes'
+        }
+      }, function (error, response, body) {
+        if (error) {
+          done(error);
+        } else {
+          response.statusCode.should.be.equal(201);
+          var bodyParsed = JSON.parse(body).data;
+//          console.log(bodyParsed);
+//          bodyParsed.product.should.be.equal(productId);
+          bodyParsed.seller.should.be.equal(sellerId);
+          bodyParsed.totalAus.should.be.equal(10);
+          bodyParsed.usedAus.should.be.equal(5);
+          bodyParsed.price.should.be.equal(1100);
+          bodyParsed.creditLimit.should.be.equal(10000);
+          bodyParsed.cashLimit.should.be.equal(10000);
+          bodyParsed.currentBalance.should.be.equal(1000);
+          bodyParsed.ncRating.should.be.equal('Silver');
+          bodyParsed.bcRating.should.be.equal('Silver');
+          bodyParsed.moRating.should.be.equal('Silver');
+          bodyParsed.cost.should.be.equal(1000);
+          bodyParsed.notes.should.be.equal('Some notes');
+          tradelineId = bodyParsed.id;
+          done();
+        }
+      });
+    });
 
-  it('seller can update one tradelines');
+    it('returns proper data for GET one by id response', function (done) {
+      request({
+        'method': 'GET',
+        'url': 'http://localhost:' + port + '/api/v1/seller/tradelines/' + tradelineId,
+        'headers': {'huntKey': sellerHuntKey}
+      }, function (error, response, body) {
+        if (error) {
+          done(error);
+        } else {
+          response.statusCode.should.be.equal(200);
+          var bodyParsed = JSON.parse(body).data;
+          bodyParsed.product.id.should.be.equal(productId);
+          bodyParsed.seller.should.be.equal(sellerId);
+          bodyParsed.totalAus.should.be.equal(10);
+          bodyParsed.usedAus.should.be.equal(5);
+          bodyParsed.price.should.be.equal(1100);
+          bodyParsed.creditLimit.should.be.equal(10000);
+          bodyParsed.cashLimit.should.be.equal(10000);
+          bodyParsed.currentBalance.should.be.equal(1000);
+          bodyParsed.ncRating.should.be.equal('Silver');
+          bodyParsed.bcRating.should.be.equal('Silver');
+          bodyParsed.moRating.should.be.equal('Silver');
+          bodyParsed.cost.should.be.equal(1000);
+          bodyParsed.notes.should.be.equal('Some notes');
+          done();
+        }
+      });
+    });
 
-  it('seller can send to archive one tradeline');
+    it('returns proper data for GET response', function (done) {
+      request({
+        'method': 'GET',
+        'url': 'http://localhost:' + port + '/api/v1/seller/tradelines',
+        'headers': {'huntKey': sellerHuntKey}
+      }, function (error, response, body) {
+        if (error) {
+          done(error);
+        } else {
+          response.statusCode.should.be.equal(200);
+          var bodyParsed = JSON.parse(body);
+          Array.isArray(bodyParsed.data).should.be.true;
+          bodyParsed.data.length.should.be.above(0);
+          bodyParsed.data.map(function (tradeline) {
+            tradeline.id.should.be.a.String;
+            tradeline.id.should.match(/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i);
+            tradeline.totalAus.should.be.below(16);
+            tradeline.usedAus.should.be.below(16);
+            tradeline.creditLimit.should.be.below(1000000);
+//          tradeline.currentBalance.should.be.below(1000000);
+            tradeline.cost.should.be.a.Number;
+            tradeline.price.should.be.a.Number;
+            tradeline.notes.should.be.a.String;
+            tradeline.seller.should.be.a.String;
+            tradeline.seller.should.match(/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i);
+            tradeline.seller.should.be.equal(sellerId);
+            tradeline.dateOpen.should.be.a.Date;
+            tradeline.product.id.should.be.a.String;
+            tradeline.product.id.should.match(/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i);
+            ['ncRating', 'bcRating', 'moRating'].map(function (r) {
+              ['None', 'Bronze', 'Silver', 'Gold'].should.containEql(tradeline[r])
+            });
+          });
+          done();
+        }
+      });
+    });
 
+    it('seller can update one tradelines');
+
+    it('seller can send to archive one tradeline');
+  });
 });
