b3743e0cd01f179f79c60fe1d07e6eb3d5ffe210
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-06-19 00:31:04 +0400

    https://oselot.atlassian.net/browse/ACR-44

--- controllers/buyer/cart.js
+++ controllers/buyer/cart.js
@@ -3,7 +3,7 @@ var ensureBuyerOrOwner = require('../middleware.js').ensureBuyerOrOwner;
 module.exports = exports = function (core) {
 
   core.app.get('/api/v1/buyer/cart', ensureBuyerOrOwner, function (request, response) {
-    var tradelineIds = request.user.profile ? (request.user.profile.cart || []) : [];
+    var tradelineIds = request.user.profile ? (request.user.profile.cart.keys() || []) : [];
     core.async.map(tradelineIds,
       request.model.TradeLine.findById,
       function (error, tradeLinesFound) {
@@ -18,4 +18,100 @@ module.exports = exports = function (core) {
       });
   });
 
+  core.app.put('/api/v1/buyer/cart/:id', ensureBuyerOrOwner, function (request, response) {
+    if (request.params.id) {
+      core.async.waterfall([
+        function (cb) {
+          request.model.TradeLine.findById(request.params.id, cb);
+        }
+      ], function (error, tradeLineFound) {
+        if (error) {
+          throw error;
+        } else {
+          if (tradeLineFound) {
+            if (tradeLineFound.active) {
+              request.user.profile = request.user.profile || {};
+              request.user.profile.cart = request.user.profile.cart || {};
+              request.user.profile.cart['' + tradeLineFound.id] = true;
+              request.user.save(function (error) {
+                if (error) {
+                  throw error;
+                } else {
+                  response.status(202);//accepted
+                  response.json({'status': 'ok'}); //todo: what is the expected response for it?
+                }
+              });
+            } else {
+              response.status(400);
+              response.json({
+                'status': 'Error',
+                'errors': [
+                  {
+                    'code': 400,
+                    'message': 'Tradeline is not available, it is not in active state!'
+                  }
+                ]
+              });
+            }
+          } else {
+            response.status(404);
+            response.json({
+              'status': 'Error',
+              'errors': [
+                {
+                  'code': 404,
+                  'message': 'Tradeline with this id do not exists!'
+                }
+              ]
+            });
+          }
+        }
+      });
+    } else {
+      response.status(400);
+      response.json({
+        'status': 'Error',
+        'errors': [
+          {
+            'code': 400,
+            'message': 'Tradeline id is missed!'
+          }
+        ]
+      });
+    }
+  });
+
+  core.app.delete('/api/v1/buyer/cart/:id', ensureBuyerOrOwner, function (request, response) {
+    if (request.params.id) {
+      if (request.user.profile && request.user.profile.cart) {
+        delete request.user.profile.cart[request.params.id];
+      } else {
+        request.user.profile = request.user.profile || {};
+        request.user.profile.cart = request.user.profile.cart || {};
+      }
+      request.user.save(function (error) {
+        if (error) {
+          throw error;
+        } else {
+          response.status(202);//accepted
+          response.json({'status': 'ok'}); //todo: what is the expected response for it?
+        }
+      });
+    } else {
+      response.status(400);
+      response.json({
+        'status': 'Error',
+        'errors': [
+          {
+            'code': 400,
+            'message': 'Tradeline id is missed!'
+          }
+        ]
+      });
+    }
+  });
+
+  core.app.post('/api/v1/buyer/cart/:id', ensureBuyerOrOwner, function (request, response) {
+    response.send('okay');
+  });
 };
