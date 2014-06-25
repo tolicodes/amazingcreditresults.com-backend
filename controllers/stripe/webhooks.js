e8d34818eb52602d06a12459f53cc8c7495cc0b7
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-06-25 20:09:42 +0400

    https://oselot.atlassian.net/browse/ACR-380

--- controllers/stripe/webhooks.js
+++ controllers/stripe/webhooks.js
@@ -2,7 +2,7 @@ var stripe = require('stripe')(process.env.STRIPE_API_KEY || 'sk_test_Bq4myUHpyX
 
 
 module.exports = exports = function (core) {
-  core.app.get('/stripewebhooks', function (request, response) {
+  core.app.post('/stripewebhooks', function (request, response) {
   //todo - change /stripewebhooks to something much more unpredictable
   //like - /papytraxaetsobakatakemuinado
     console.info(request.body);
