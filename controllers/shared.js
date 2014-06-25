2d1cc6f1fdc73a42aefa8c1f1e40e73e3a981bc5
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-06-25 21:13:46 +0400

    https://oselot.atlassian.net/browse/ACR-51

--- controllers/shared.js
+++ controllers/shared.js
@@ -1,4 +1,5 @@
-var  formatUser = require('./formatter.js').formatUserForOwner;
+var formatUser = require('./formatter.js').formatUserForOwner,
+  ensureBuyerOrOwner = require('./../lib/middleware.js').ensureBuyerOrOwner;
 
 //GET request to get current authorized users parameters in form of json
 module.exports = exports = function (core) {
@@ -13,4 +14,9 @@ module.exports = exports = function (core) {
 
   core.app.all('/api/v1/myself', f4myself);
   core.app.all('/auth/myself', f4myself); //not used and can be deprecated
+
+//https://oselot.atlassian.net/browse/ACR-51
+  core.app.put('/api/v1/myself', ensureBuyerOrOwner, function (request, response) {
+    response.redirec('https://oselot.atlassian.net/browse/ACR-51');
+  });
 };
