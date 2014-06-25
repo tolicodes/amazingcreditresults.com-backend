fd6de6039151cab897f68342f452be082c0c843c
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-06-26 00:52:18 +0400

    https://oselot.atlassian.net/browse/ACR-387#

--- controllers/shared.js
+++ controllers/shared.js
@@ -8,7 +8,15 @@ module.exports = exports = function (core) {
       response.json(formatUser(request.user));
     } else {
       response.status(400);
-      response.json({'Error': 'Authorization required!'});
+      response.json({
+        'status': 'Error',
+        'errors': [
+          {
+            'code': 400,
+            'message': 'Authorization required!'
+          }
+        ]
+      });
     }
   }
 
@@ -18,5 +26,44 @@ module.exports = exports = function (core) {
 //https://oselot.atlassian.net/browse/ACR-51
   core.app.put('/api/v1/myself', ensureBuyerOrOwner, function (request, response) {
     response.redirect('https://oselot.atlassian.net/browse/ACR-51');
+  });
+
+//https://oselot.atlassian.net/browse/ACR-387#
+  core.app.get('/api/v1/account', function (request, response) {
+    if (request.user) {
+      request.model.Transaction.find({'client': request.user._id})
+        .sort('+timestamp')
+        .exec(function (error, transactionsFound) {
+          var balance = 0,
+            transactionsFormatted = [];
+          transactionsFound.map(function (t) {
+            transactionsFormatted.push({
+              'id': t.id,
+              'timestamp': t.timestamp,
+              'amount': t.amount,
+              'type': t.type
+            });
+            balance = balance + t.amount;
+          });
+
+          response.json({
+            'data': {
+              'balance': balance,
+              'transactions': transactionsFormatted
+            }
+          });
+        });
+    } else {
+      response.status(400);
+      response.json({
+        'status': 'Error',
+        'errors': [
+          {
+            'code': 400,
+            'message': 'Authorization required!'
+          }
+        ]
+      });
+    }
   });
 };
