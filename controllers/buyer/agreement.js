c1e5b7966063f249cb5bea97596ebd35c96bad67
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-07-15 22:40:57 +0400

    https://oselot.atlassian.net/browse/ACR-266

--- controllers/buyer/agreement.js
+++ controllers/buyer/agreement.js
@@ -1,6 +1,7 @@
 var ensureBuyerOrOwner = require('../../lib/middleware.js').ensureBuyerOrOwner,
-  DoSign = require('echosign');
-
+  DoSign = require('echosign'),
+  os = require('os'),
+  wkhtmltopdf = require('wkhtmltopdf');
 
 
 module.exports = exports = function (core) {
@@ -8,16 +9,131 @@ module.exports = exports = function (core) {
     applicationCredentials = core.config.echoSign.auth.applicationCredentials,
     doSign = new DoSign()
       .setUserCredentials(userCredentials.email, userCredentials.password, userCredentials.apiKey)
-      .setApplicationCredentials(applicationCredentials.applicationId, applicationCredentials.applicationSecret)
-    ;
+      .setApplicationCredentials(applicationCredentials.applicationId, applicationCredentials.applicationSecret);
+
   core.app.post('/api/v1/buyer/getAgreement', ensureBuyerOrOwner, function (request, response) {
-    response.json({
-      'user': request.user,
-      'body': request.body
-    });
+    var docName = 'agreement_' + request.user.name.givenName + '_' + request.user.name.familyName,
+      transientDocumentId;
+
+    core.async.waterfall(
+      [
+        function (cb) {
+          core.app.render('agreements/checkoutAgreement', {
+            'layout': false,
+            'user': request.user,
+            'date': new Date()
+          }, cb);
+        },
+        function (agreementInHtml, cb) {
+          wkhtmltopdf(agreementInHtml, { output: os.tmpdir() + '/' + docName + '.pdf' }, function (error) {
+            console.log('Agreement saved in ' + os.tmpdir() + '/' + docName + '.pdf');
+            cb(error);
+          });
+        },
+        function (cb) {
+          doSign.uploadTransientDocument(os.tmpdir() + '/' + docName + '.pdf', cb);
+        },
+        function (id, cb) {
+          transientDocumentId = id;
+          doSign.doRequest({
+            'json': {
+              'documentCreationInfo': {
+                'signatureType': 'ESIGN',
+                'formFieldLayerTemplates': [
+                  {
+//                    'libraryDocumentId': 'string',
+                    'transientDocumentId': transientDocumentId,
+//                    'libraryDocumentName': 'string',
+//                    'documentURL': {
+//                      'name': 'string',
+//                      'url': 'string',
+//                      'mimeType': 'string'
+//                    }
+                  }
+                ],
+//                'callbackInfo': 'string', //where to upload signed document?
+//                'securityOptions': {
+//                  'passwordProtection': 'string',
+//                  'kbaProtection': 'string',
+//                  'webIdentityProtection': 'string',
+//                  'protectOpen': 'boolean',
+//                  'internalPassword': 'string',
+//                  'externalPassword': 'string',
+//                  'openPassword': 'string'
+//                },
+                'recipients': [
+                  {
+//                    'fax': 'string',
+                    'email': request.user.email,
+                    'role': 'SIGNER'
+                  }
+                ],
+//                'daysUntilSigningDeadline': 'Integer',
+//                'locale': 'string',
+//                'ccs': [
+//                  'string'
+//                ],
+//                'vaultingInfo': {
+//                  'enabled': 'boolean'
+//                },
+//                'signatureFlow': 'string',
+//                'message': 'string',
+//                'mergeFieldInfo': [
+//                  {
+//                    'defaultValue': 'string',
+//                    'fieldName': 'string'
+//                  }
+//                ],
+//                'fileInfos': [
+//                  {
+//                    'libraryDocumentId': 'string',
+//                    'transientDocumentId': 'string',
+//                    'libraryDocumentName': 'string',
+//                    'documentURL': {
+//                      'name': 'string',
+//                      'url': 'string',
+//                      'mimeType': 'string'
+//                    }
+//                  }
+//                ],
+//                'reminderFrequency': 'string',
+//                'name': 'string'
+//              },
+//                'options': {
+//                  'noChrome': 'boolean',
+//                  'authoringRequested': 'boolean',
+//                  'autoLoginUser': 'boolean'
+//                }
+              }
+            }
+          }, function (error, response, body) {
+            if (error) {
+              cb(error)
+            } else {
+              if (response.statusCode >= 400) {
+                cb(new Error('Error - ' + response.statusCode + ' ' + body.code + ' ' + body.message));
+              } else {
+                cb(null, body);
+              }
+            }
+          });
+        }
+      ],
+      function (error, echoSignResponse) {
+        if (error) {
+          throw error;
+        } else {
+          response.json({
+            'transientDocumentId': transientDocumentId,
+            'user': request.user,
+            'echoSignResponse': echoSignResponse //todo - save it into profile?
+          });
+        }
+      }
+    );
   });
 
-  core.app.post('/api/v1/buyer/signAgreement', ensureBuyerOrOwner, function (request, response) {
+  core.app.put('/api/v1/buyer/signAgreement', ensureBuyerOrOwner, function (request, response) {
     response.json({
       'user': request.user,
       'body': request.body
