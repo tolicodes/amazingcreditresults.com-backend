dba042aa34518b611fb4f7494c9de25b51898066
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-08-06 00:33:29 +0400

    http://jira.oselot.com/browse/ACR-488

--- controllers/buyer/verifications/evs.js
+++ controllers/buyer/verifications/evs.js
@@ -10,25 +10,49 @@ module.exports = exports = function (core) {
       if (request.user.profile.ssnVerified) {
         utilities.error(400, 'SSN code is already verified!', response);
       } else {
-//todo: caching
-        var builder = new xml2js.Builder(),
-          content = builder.buildObject({
-            'PlatformRequest': {
-              'Credentials': {
-                'Username': core.config.evs.username,
-                'Password': core.config.evs.password
-              },
-              'CustomerReference': request.user.id,
-              'Identity': {
-                'Ssn': request.user.profile.ssn
+        var retryAfterMs = (24 * 60 * 60 * 1000 / 3),  //3 times per day
+          retryAfter = Date.now() - (request.user.profile.ssnVerificationTry) ? (request.user.profile.ssnVerificationTry.getTime()) : (Date.now());
+        if (retryAfter < retryAfterMs) {
+//perform verification
+          var builder = new xml2js.Builder(),
+            content = builder.buildObject({
+              'PlatformRequest': {
+                'Credentials': {
+                  'Username': core.config.evs.username,
+                  'Password': core.config.evs.password
+                },
+                'CustomerReference': request.user.id,
+                'Identity': {
+                  'Ssn': request.user.profile.ssn
+                }
               }
+            });
+          curl({
+            'method': 'POST',
+            'url': 'https://identiflo.everification.net/WebServices/Integrated/Main/V200/SSNLookup',
+            'body': content
+          }, function (error, response, body) {
+            if (error) {
+              throw error;
+            } else {
+//if names matches, we verify account
             }
           });
-
-
+        } else {
+//rate limit
+          response.status(400);
+          response.json({
+            'status': 'error',
+            'errors': [
+              {
+                'code': 400,
+                'message': 'SSN verification rate limited'
+              }
+            ],
+            'retryAfter': retryAfter
+          });
+        }
       }
-
-
     } else {
       utilities.error(400, 'SSN code is not entered!', response);
     }
